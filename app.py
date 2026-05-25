3714757f6885b62b521bbc048b68cec9ab92c5bd
hasinikalvakk-lang <hasinikalvakk@gmail.com>
2026-05-25 14:30:03 +0530

    Fixed file upload path issue

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, request
 import os
+
 from utils.pdf_parser import extract_text_from_pdf
 from utils.skill_extractor import extract_skills
 from utils.ats_score import calculate_ats_score
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/tmp'
+# Upload folder
+UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Create uploads folder if not exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
@@ -25,12 +32,16 @@ def analyze():
         return 'No selected file'
 
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+
     file.save(filepath)
 
+    # Extract text
     resume_text = extract_text_from_pdf(filepath)
 
+    # Extract skills
     skills = extract_skills(resume_text)
 
+    # Calculate ATS score
     ats_score = calculate_ats_score(skills)
 
     return render_template(
@@ -39,6 +50,7 @@ def analyze():
         ats_score=ats_score,
         resume_text=resume_text
     )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
